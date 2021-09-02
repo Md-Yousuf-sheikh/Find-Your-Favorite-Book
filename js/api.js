34901b4a732e680c3d228885cee61bafc325b24e
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2021-09-02 18:43:55 +0600

    secent update

--- js/api.js
+++ js/api.js
@@ -2,8 +2,9 @@
 const searchBook = () => {
     const searchField = document.getElementById('searchInput');
     const searchText = searchField.value;
+    
     // spinner show
-    // Spinner('block')
+    Spinner('block')
 
     // clear data
     searchField.value = '';
@@ -18,6 +19,7 @@ const searchBook = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data.docs))
+            inputEmpty('none');
     }
 }
 
@@ -25,40 +27,45 @@ const searchBook = () => {
 const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    //  error message
+    //  error-message message
     if (books.length === 0) {
         resultErrer('block')
     }else{
         resultErrer('none')
     }
+    // Total Search Result 
+        document.getElementById('total-search-result').innerText = books.length;
+    //  console.log(books.length);
+    
     // forEach books
     books.forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
-        const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`
-        const imgDefolt = 'http://www.friendlyautosale.net/application/modules/themes/views/default/assets/images/image-placeholder.png' 
+        const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`;
+
         div.innerHTML = `
             <div class="card-body h-25">
                 <img src="${imgUrl}" class="card-img-top" alt="...">
                 <h3 class="card-title">Book: ${book.title}</h3>
-                <h4 class="card-title">Author: ${book.author_name[0]}</h4>
+                <h4 class="card-title">Author: ${book.author_name}</h4>
                 <h5 class="card-title">Publish: ${book.first_publish_year}</h5>
             </div>
         `;
         searchResult.appendChild(div);
+        Spinner('none');
     });
     
-}
+};
 // Input Empty
 const inputEmpty = (inputDes)=>{
     document.getElementById('input-empty').style.display=inputDes;
 }
-// result search  error 
+// result search  error-message 
 const resultErrer = dsplayStyle =>{
-    document.getElementById('error').style.display =dsplayStyle;
+    document.getElementById('error-message').style.display =dsplayStyle;
 }
 
-// // Spinner 
-// const Spinner = dsplay=>{
-//     document.getElementById('spinner').style.display =dsplay;
-// }
+// Spinner 
+const Spinner = dsplay=>{
+    document.getElementById('spinner').style.display =dsplay;
+}
